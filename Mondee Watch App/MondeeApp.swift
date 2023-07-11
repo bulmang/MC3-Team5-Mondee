12fa736ae7497d418e6697fe5ab10efb500da868
hyunjuntyler <hyunjuntyler@gmail.com>
2023-07-11 12:14:37 +0900

    Initial Commit

---
+++ Mondee Watch App/MondeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MondeeApp.swift
+//  Mondee Watch App
+//
+//  Created by hyunjun on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct Mondee_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mondee/MondeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MondeeApp.swift
+//  Mondee
+//
+//  Created by hyunjun on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct MondeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
