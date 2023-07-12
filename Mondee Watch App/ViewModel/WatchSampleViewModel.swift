4ce70d85eff4a9d9b58c9a6e9cda209a0fb883f2
hyunjuntyler <hyunjuntyler@gmail.com>
2023-07-12 21:05:30 +0900

    add: MVVM 폴더 구조 추가

---
+++ Mondee Watch App/ViewModel/WatchSampleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WatchSampleViewModel.swift
+//  Mondee Watch App
+//
+//  Created by hyunjun on 2023/07/12.
+//
+
+import Foundation
---
+++ Mondee/Model/SampleModel.swift
@@ -0,0 +1,8 @@
+//
+//  SampleModel.swift
+//  Mondee
+//
+//  Created by hyunjun on 2023/07/12.
+//
+
+import Foundation
