26ed4e558478cd7d1d24a1e3edb49e29d5abe539
bulmang <hmk113737@gmail.com>
2023-07-14 18:27:09 +0900

    [watchOS] feat: User가 Crown을 다 돌렸을때 GameGuideView 올라옴 (#3)

---
+++ Mondee Watch App/View/BeforeGameView.swift
@@ -0,0 +1,107 @@
+//
+//  BeforeGameView.swift
+//  Mondee Watch App
+//
+//  Created by 하명관 on 2023/07/14.
+//
+
+import SwiftUI
+
+struct BeforeGameView: View {
+    
+    // MARK: Properties
+    
+    @Binding var mondeeScroll: CGFloat
+    @Binding var isGuideActive: Bool
+    
+    private let topScrollLimit = 250
+    private let mondeePadding = 100
+    private let mondeeFinalLocationRatio = 2.5
+    
+    let bottomScrollLimit : CGFloat
+    
+    var body: some View {
+        GeometryReader { geo in
+            
+            let deviceHeight = geo.size.height
+            let deviceWidth = geo.size.width
+            let mondeeWidth = geo.size.width - CGFloat(mondeePadding)
+            
+            NavigationView {
+                ZStack(alignment: .top) {
+                    
+                    // MARK: MONDEE
+                    
+                    VStack() {
+                        Image("ImageWatchMondee")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: mondeeWidth)
+                            .offset(y: startGame ? deviceHeight/mondeeFinalLocationRatio : -((self.mondeeScroll)/6))
+                            .focusable(isFocusable)
+                            .digitalCrownRotation($mondeeScroll, from: CGFloat(bottomScrollLimit), through:CGFloat(topScrollLimit))
+                    }
+                    .navigationTitle {
+                        HStack {
+                            Text("Mondee")
+                                .foregroundColor(.mint)
+                                .fontDesign(.rounded)
+                                .fontWeight(.heavy)
+                            Spacer()
+                        }
+                    }
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbarBackground(.hidden)
+                    .ignoresSafeArea(.all)
+                    
+                    // MARK: BATH IMAGE
+                    
+                    VStack {
+                        Spacer()
+                        Image("ImageWatchBath")
+                            .resizable()
+                            .frame(maxWidth: deviceWidth)
+                    }
+                    
+                    // MARK: ARROW ANIMATION
+                    
+                    HStack {
+                        Spacer()
+                        ArrowAnimation()
+                    }
+                    .opacity(opacityValue)
+                    
+                }
+                .padding(.top,50)
+                .ignoresSafeArea()
+            }
+            .sheet(isPresented: $isGuideActive) {
+                GameGuideView()
+                    .toolbar(content: {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button {
+                                mondeeScroll = 0
+                                self.isGuideActive = false
+                            } label: {
+                                Text("취소")
+                            }
+                        }
+                    })
+            }
+        }
+    }
+    
+    private var startGame: Bool {
+        return  mondeeScroll <= CGFloat(bottomScrollLimit)
+    }
+    
+    private var isFocusable: Bool {
+        return mondeeScroll > bottomScrollLimit
+    }
+    
+    private var opacityValue: Double {
+        return mondeeScroll <= bottomScrollLimit ? 0 : 1
+    }
+    
+}
+
