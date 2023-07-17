f951f8be2dab233d6b8930256ba7c04c7d1101eb
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2023-07-17 09:43:24 +0900

    [watchOS] chore: constants값 수정 (#5)

--- Mondee Watch App/Constants.swift
+++ Mondee Watch App/Constants.swift
@@ -8,7 +8,7 @@
 struct Constants {
     static let initialSeconds = 600
     static let initialHeartCount = 3
-    static let motionlessThreshold1 = 20 // 경고 임계치
-    static let motionlessThreshold2 = 25 // 캐릭터가 더러워지는 임계치
-    static let motionlessThreshold3 = 20 // 캐릭터가 깨끗해지는 임계치
+    static let motionlessThreshold1 = 20 // 경고 임계값
+    static let motionlessThreshold2 = 25 // 캐릭터가 더러워지는 임계값
+    static let movingThreshold = 10 // 캐릭터가 다시 깨끗해지는 임계값
 }
