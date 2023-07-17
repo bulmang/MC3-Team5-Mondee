5927604b5c9a7be51d43ce572fe916e8ae350361
seunghoria <seungeun991114@gmail.com>
2023-07-17 11:46:33 +0900

    [watchOS] feat: 게임 시작 카운트 다운 전 텍스트 추가 (#4)

--- Mondee Watch App/View/GameView.swift
+++ Mondee Watch App/View/GameView.swift
@@ -17,13 +17,14 @@ struct GameView: View {
     @State private var isGuideActive: Bool = false
     @State private var isGameStartActive: Bool = false
     
+    
     private let bottomScrollLimit: CGFloat = -270
     private let gameStartScroll: CGFloat = -280
     
     var body: some View {
         
         startGameOnCharacterReachedBath()
-
+        
         // TODO: 추후 print 코드 제거 요망
         let _ = print("mondeeScroll : \(mondeeScroll)")
         
@@ -33,7 +34,7 @@ struct GameView: View {
         case .playingGame:
             return AnyView(Text("Playing"))
         case .afterGame:
-            return AnyView(GameGuideView().transition(.move(edge: .bottom)))
+            return AnyView(GameGuideView(isGuideActive: $isGuideActive).transition(.move(edge: .bottom)))
         }
     }
     
@@ -58,15 +59,46 @@ struct GameView: View {
 }
 
 
+struct GameGuideView: View {
+    @State private var showGameCountView = false
+    @Binding var isGuideActive: Bool
+    
+    var body: some View{
+        VStack{
+            if showGameCountView {
+                BeforeGameCountView(isGuideActive: $isGuideActive)
+            } else {
+                HStack{
+                    Image("ImageWatchBubbles")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 27)
+                    Text("청소시간")
+                        .font(.system(size: 20, weight: .semibold))
+                    Image("ImageWatchBubble")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 24)
+                }
+                .padding(.bottom, 10)
+                Text("몸을 움직여\n먼지를 씻겨주자")
+                    .font(.system(size: 15, weight: .regular))
+                    .multilineTextAlignment(.center)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                showGameCountView = true
+            }
+        }
+        
+    }
+}
+
+
+
 struct StartGame_Previews: PreviewProvider {
     static var previews: some View {
         GameView()
     }
 }
-
-struct GameGuideView: View {
-    var body: some View{
-        Text("Guide")
-    }
-}
-
