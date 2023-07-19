38a8e05e36bf80e66ed4243e12ee02cfbbcb5e03
hyunjun <hyunjuntyler@gmail.com>
2023-07-19 10:13:44 +0900

    [watchOS] design: 타이머 폰트 모디파이어 추가 (#34)

--- Mondee Watch App/View/Modifier/Modifier.swift
+++ Mondee Watch App/View/Modifier/Modifier.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct BubbleFont: View {
     var body: some View {
-        ZStack {
-            Color.black
+        TabView {
+            Text("05:30")
+                .timerFontModifier()
+                .padding()
+                .padding(.horizontal)
+                .background(
+                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                )
             Text("2")
                 .font(.system(size: 150))
-                .modifier(BubbleFontModifier())
+                .bubbleFontModifier()
         }
     }
 }
@@ -26,12 +32,31 @@ struct BubbleFontModifier: ViewModifier {
             .foregroundStyle(
                 Color.black
                     .shadow(.inner(color: .purple.opacity(0.4), radius: 2, x: 4, y: 4))
-                .shadow(.inner(color: .blue, radius: 2, x: -2, y: 2))
-                .shadow(.inner(color: .yellow.opacity(0.4), radius: 4, x: 1.5, y: -2))
-                .shadow(.inner(color: .white.opacity(0.5), radius: 4, x: 0, y: 8))
-                .shadow(.inner(color: .white, radius: 1))
+                    .shadow(.inner(color: .blue, radius: 2, x: -2, y: 2))
+                    .shadow(.inner(color: .yellow.opacity(0.4), radius: 4, x: 1.5, y: -2))
+                    .shadow(.inner(color: .white.opacity(0.5), radius: 4, x: 0, y: 8))
+                    .shadow(.inner(color: .white, radius: 1))
             )
-            
+    }
+}
+
+struct TimerFontModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .foregroundColor(.black)
+            .fontDesign(.rounded)
+            .fontWeight(.semibold)
+            .shadow(radius: 2, x: 2, y: 2)
+    }
+}
+
+extension View {
+    func bubbleFontModifier() -> some View {
+        modifier(BubbleFontModifier())
+    }
+    func timerFontModifier() -> some View {
+        modifier(TimerFontModifier())
     }
 }
 
