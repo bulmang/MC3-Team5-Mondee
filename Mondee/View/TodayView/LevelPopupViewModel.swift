8e0cfa4921df12ff463a5929d92d86d419f36356
Woozu96 <yejuzzang0906@gmail.com>
2023-07-26 11:19:44 +0900

    [IOS] Feat: 레벨팝업뷰 모델 구현 (#46)

---
+++ Mondee/View/TodayView/LevelPopupViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  levelPopupViewModel.swift
+//  Mondee
+//
+//  Created by woozu on 2023/07/25.
+//
+
+import Foundation
+struct LevelPopupViewModel : Identifiable {
+    var id = UUID()
+    
+    let mondeeImg: String
+    let mondeeTitle: String
+    let mondeeContents: String
+}
