72f39a963cbcf4e1d81b1a2db48b0fe6b724ab16
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2023-07-28 21:46:18 +0900

    [iOS] data: 데이터 뷰모델 수정 (#80)

--- Mondee/ViewModel/RecordViewModel.swift
+++ Mondee/ViewModel/RecordViewModel.swift
@@ -5,4 +5,67 @@
 //  Created by Hyunjun Kim on 2023/07/28.
 //
 
-import Foundation
+import SwiftUI
+
+// MARK: - Extension for Counting Game Success and Game Fail
+extension UserData {
+    /// gameSuccess가 true인 원소 수를 가져옴
+    func countGameSuccess() -> Int {
+        return userdata.reduce(0) { count, user in
+            return count + (user.gameSuccess ? 1 : 0)
+        }
+    }
+
+    /// gameFail이 true인 원소 수를 가져옴
+    func countGameFail() -> Int {
+        return userdata.reduce(0) { count, user in
+            return count + (user.gameFail ? 1 : 0)
+        }
+    }
+
+    /// 이번 달의 게임 성공 횟수를 가져옴
+    func countGameSuccessThisMonth() -> Int {
+        let currentDate = Date()
+        return userdata.reduce(0) { count, user in
+            let calendar = Calendar.current
+            let isThisMonth = calendar.isDate(user.gamePlayDate, equalTo: currentDate, toGranularity: .month)
+            return count + (user.gameSuccess && isThisMonth ? 1 : 0)
+        }
+    }
+
+    /// 이번 달의 게임 실패 횟수를 가져옴
+    func countGameFailThisMonth() -> Int {
+        let currentDate = Date()
+        return userdata.reduce(0) { count, user in
+            let calendar = Calendar.current
+            let isThisMonth = calendar.isDate(user.gamePlayDate, equalTo: currentDate, toGranularity: .month)
+            return count + (user.gameFail && isThisMonth ? 1 : 0)
+        }
+    }
+
+    /// 총 플레이 타임을 가져옴
+    func totalGamePlayTime() -> Int {
+        return userdata.reduce(0) { totalPlayTime, user in
+            return totalPlayTime + user.gamePlayTime
+        }
+    }
+
+    /// 이번 달의 총 플레이 타임을 가져옴
+    func gamePlayTimeThisMonth() -> Int {
+        let currentDate = Date()
+        return userdata.reduce(0) { totalPlayTime, user in
+            let calendar = Calendar.current
+            let isThisMonth = calendar.isDate(user.gamePlayDate, equalTo: currentDate, toGranularity: .month)
+            return totalPlayTime + (isThisMonth ? user.gamePlayTime : 0)
+        }
+    }
+    
+    /// 게임 성공 날짜가 있는지 판별하는 함수
+    func isSuccessfulDate(date: Date) -> Bool {
+        let successfulDates = userdata.filter { $0.gameSuccess }.map { $0.gamePlayDate }
+        return successfulDates.contains { successfulDate in
+            Calendar.current.isDate(successfulDate, inSameDayAs: date)
+        }
+    }
+}
+
