2425396cce7d238b2317e64886e9c0af084ccca8
seunghoria <seungeun991114@gmail.com>
2023-07-31 12:28:55 +0900

    [iOS] feat: 공유카드 먼디 이미지 최상단 위치 (#93)

--- Mondee/View/TodayView/SharingCardView.swift
+++ Mondee/View/TodayView/SharingCardView.swift
@@ -15,16 +15,17 @@ struct SharingCardView: View {
     
     var body: some View {
         ZStack {
-            if selection == 0 {
-                SharingCard1View(cleaningTime: cleaningTime)
-            } else if selection == 1 {
-                SharingCard2View()
-            }
             Image(viewModel.todayMondee?.mondeeImg ?? viewModel.currentLevel.mondeeImg)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 200)
                 .padding(.top)
+            
+            if selection == 0 {
+                SharingCard1View(cleaningTime: cleaningTime)
+            } else if selection == 1 {
+                SharingCard2View()
+            }
         }
     }
 }
